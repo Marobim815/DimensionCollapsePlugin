34bef205ce25fd9337444347dece97689a66d7e5
Marobim815 <be1wer0815@gmail.com>
2024-11-17 22:44:49 +0900

    이벤트 핸들러, 팀 코어 추가

--- src/main/kotlin/com/github/marobim815/dimensionPlugin/DimensionPlugin.kt
+++ src/main/kotlin/com/github/marobim815/dimensionPlugin/DimensionPlugin.kt
@@ -1,6 +1,10 @@
 package com.github.marobim815.dimensionPlugin
 
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class DimensionPlugin : JavaPlugin(), Listener {
@@ -8,6 +12,27 @@ class DimensionPlugin : JavaPlugin(), Listener {
     override fun onEnable() {
         logger.info("플러그인 활성화!!")
         server.pluginManager.registerEvents(this, this)
+    }
+
+    @EventHandler
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        val player = event.player
+        player.sendTitle(
+            "§6§l환영합니다!!",
+            "§a$player",
+            10, 70, 20
+        )
+    }
+
+    @EventHandler
+    fun onBlockPlace(event: BlockPlaceEvent) {
+        val player = event.player
+        val block = event.block
+
+        if (block.type == Material.BEDROCK) {
+            val location = block.location
+            val world = block.world
+        }
     }
 
     override fun onDisable() {
--- src/main/kotlin/com/github/marobim815/dimensionPlugin/TeamCore.kt
+++ src/main/kotlin/com/github/marobim815/dimensionPlugin/TeamCore.kt
@@ -1,4 +1,30 @@
 package com.github.marobim815.dimensionPlugin
 
+import org.bukkit.Location
+import org.bukkit.World
+import org.bukkit.entity.Slime
+import org.bukkit.potion.PotionEffect
+
 class TeamCore {
+    fun spawnCore(
+        location: Location,
+        world: World,
+        size: Int = 5,
+        customName: String? = null,
+        effects: List<PotionEffect> = emptyList()
+    ): Slime {
+        val core = world.spawn(location, Slime::class.java)
+
+        if (customName != null) {
+            core.customName =customName
+            core.isCustomNameVisible = true
+        }
+
+        effects.forEach { core.addPotionEffect(it) }
+        core.setAI(false)
+        core.setGravity(false)
+        core.health = 2000.0
+        core.size = size
+        return core
+    }
 }
