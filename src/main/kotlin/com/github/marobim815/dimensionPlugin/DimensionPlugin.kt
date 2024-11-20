7ec290604b17d90c2414576ef8d207818f10e1f1
Marobim815 <be1wer0815@gmail.com>
2024-11-20 15:08:45 +0900

    카드 시스템 등록

--- src/main/kotlin/com/github/marobim815/dimensionPlugin/DimensionPlugin.kt
+++ src/main/kotlin/com/github/marobim815/dimensionPlugin/DimensionPlugin.kt
@@ -1,14 +1,14 @@
 package com.github.marobim815.dimensionPlugin
 
-import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.Sound
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.util.ChatPaginator.ChatPage
 
 class DimensionPlugin : JavaPlugin(), Listener {
 
@@ -39,9 +39,36 @@ class DimensionPlugin : JavaPlugin(), Listener {
                 player.playSound(location, Sound.ENTITY_PLAYER_ATTACK_CRIT, 1.0f, 1.0f)
             }
         } else {
-            player.sendMessage("${ChatColor.RED}코어는 y좌표 0~60사이에 설치 가능합니다!!")
+            player.sendMessage("§c코어는 y좌표 0~60사이에 설치 가능합니다!!")
             player.playSound(location, Sound.BLOCK_ANVIL_DESTROY, 1.0f, 1.0f)
             event.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun onPlayerInteract(event: PlayerInteractEvent) {
+        val player = event.player
+        val item = player.inventory.itemInMainHand
+
+        //TODO: 카드 관련 아이템 수정
+        if (item.type == Material.PAPER && item.itemMeta?.displayName == "§e팀원 생성 카드") {
+            event.isCancelled = true
+            val success = CardManager.useTeamMemberCard(player)
+            if (success) {
+                player.inventory.itemInMainHand.subtract()
+            }
+        } else if (item.type == Material.DIAMOND && item.itemMeta?.displayName == "§b의문의 카드 조각") {
+            event.isCancelled = true
+            val hasEnoughPieces = player.inventory.containsAtLeast(ItemStack(Material.DIAMOND), 9)
+            if (hasEnoughPieces) {
+                player.inventory.removeItem(ItemStack(Material.DIAMOND, 9))
+                val combinedCard = CardManager.combineCardPieces(player)
+                player.inventory.addItem(combinedCard)
+                player.sendMessage("§a강화 카드를 얻었습니다!")
+
+            } else {
+                player.sendMessage("§c카드 조각이 부족합니다! (필요: 9개)")
+            }
         }
     }
 
